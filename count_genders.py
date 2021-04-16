a25ce0ee2d80731228db6effe547e8cbe31ce6d5
Bergen Thomson <thoms344@umn.edu>
2021-04-16 17:16:03 -0500

    Got summary chart mostly complete

--- count_genders.py
+++ count_genders.py
@@ -8,22 +8,20 @@ numMenSports = 0
 numWomenSports = 0
 numTotalSports = 0
 for i in range(len(coachGenders)):
-    if "Men" in sports[i] or sports[i] in ["Football", "Baseball", "Mixed Rifle"]:
+    if "Men" not in sports[i] and "Women" not in sports[i] and sports[i] not in ["Mixed Rifle", "Football", "Baseball", "Field Hockey", "Softball"]:
+        print(sports[i])
+    if sports[i] == "Mixed Rifle":
+        continue
+    if "Men" in sports[i] or sports[i] in ["Football", "Baseball"]:     # Mixed rifle?
         coachGenderSportGender.append((coachGenders[i], "men"))
         numMenSports += 1
-    if "Women" in sports[i] or sports[i] in ["Field Hockey", "Softball", "Mixed Rifle"]:
+    if "Women" in sports[i] or sports[i] in ["Field Hockey", "Softball"]:
         coachGenderSportGender.append((coachGenders[i], "women"))
         numWomenSports += 1
     numTotalSports += 1
 
-
 menCoaches = 0
 womenCoaches = 0
-for g in coachGenders:
-    if g == "male":
-        menCoaches += 1
-    if g == "female":
-        womenCoaches += 1
 
 menCoachingMenSports = 0
 menCoachingWomenSports = 0
@@ -31,12 +29,16 @@ womenCoachingMenSports = 0
 womenCoachingWomenSports = 0
 for t in coachGenderSportGender:
     if t[0] == "male" and t[1] == "men":
+        menCoaches += 1
         menCoachingMenSports += 1
     if t[0] == "male" and t[1] == "women":
+        menCoaches += 1
         menCoachingWomenSports += 1
     if t[0] == "female" and t[1] == "men":
+        womenCoaches += 1
         womenCoachingMenSports += 1
     if t[0] == "female" and t[1] == "women":
+        womenCoaches += 1
         womenCoachingWomenSports += 1
 
 print("Number of Sports Coached by Men: " + str(menCoaches))
@@ -51,15 +53,15 @@ print("Number of Women's Sports Coached by Women: " + str(womenCoachingWomenSpor
 # summaryDataFrame.index = ['Man Head Coach', 'Woman Head Coach']
 # summaryDataFrame.to_csv("data/summary_stats.csv")
 
-menSportManCoachPercentage = (menCoachingMenSports/numMenSports) * 100
-menSportWomanCoachPercentage = (womenCoachingMenSports/numMenSports) * 100
+menSportManCoachPercentage = round((menCoachingMenSports/numMenSports) * 100, 2)
+menSportWomanCoachPercentage = round((womenCoachingMenSports/numMenSports) * 100, 2)
 
-womenSportManCoachPercentage = (menCoachingWomenSports/numWomenSports) * 100
-womenSportWomanCoachPercentage = (womenCoachingWomenSports/numWomenSports) * 100
+womenSportManCoachPercentage = round((menCoachingWomenSports/numWomenSports) * 100, 2)
+womenSportWomanCoachPercentage = round((womenCoachingWomenSports/numWomenSports) * 100, 2)
 
-totalWomenCoachPercentage = ((womenCoachingMenSports + womenCoachingWomenSports)/numTotalSports) * 100
-totalMenCoachPercentage = ((menCoachingMenSports + menCoachingWomenSports)/numTotalSports) * 100
+totalWomenCoachPercentage = round(((womenCoachingMenSports + womenCoachingWomenSports)/numTotalSports) * 100, 2)
+totalMenCoachPercentage = round(((menCoachingMenSports + menCoachingWomenSports)/numTotalSports) * 100, 2)
 
 
-summaryDataFrame = pd.DataFrame(data = { 'Sport Gender': ['Men\'s Sports', 'Men\'s Sports', 'Women\'s Sports', 'Women\'s Sports', 'Overall', 'Overall'], 'Coach Gender': ['Man', 'Woman', 'Man', 'Woman', 'Man', 'Woman'], 'Number': [menCoachingMenSports, womenCoachingMenSports, menCoachingWomenSports, womenCoachingWomenSports, menCoaches, womenCoaches], 'Percentage': [menSportManCoachPercentage, menSportWomanCoachPercentage, womenSportManCoachPercentage, womenSportWomanCoachPercentage, totalMenCoachPercentage, totalWomenCoachPercentage] })
+summaryDataFrame = pd.DataFrame(data = { 'Sport Gender': ['Men\'s Sports', 'Men\'s Sports', 'Women\'s Sports', 'Women\'s Sports', 'Overall', 'Overall'], 'Coach Gender': ['Woman', 'Man', 'Woman', 'Man', 'Woman', 'Man'], 'Number': [womenCoachingMenSports, menCoachingMenSports, womenCoachingWomenSports, menCoachingWomenSports, womenCoaches, menCoaches], 'Percentage': [menSportWomanCoachPercentage, menSportManCoachPercentage, womenSportWomanCoachPercentage, womenSportManCoachPercentage, totalWomenCoachPercentage, totalMenCoachPercentage] })
 summaryDataFrame.to_csv("data/summary_stats.csv", index=False)
--- summary_stats_stacked_bar.r
+++ summary_stats_stacked_bar.r
@@ -1,11 +1,23 @@
 library(ggplot2)
+library(plyr)
+library(scales)
 
 summaryData = read.csv(file="data/summary_stats.csv", header = TRUE)
+summaryData$Percentage <- as.numeric(summaryData$Percentage)
+summaryData <- ddply(summaryData, .(Sport.Gender), transform, pos = cumsum(Percentage) - 0.5*Percentage)
 print(summaryData)
 
-ggplot(summaryData, aes(x = Sport.Gender, y = Percentage, fill = Coach.Gender)) + 
-  geom_bar(position = "fill", stat="identity") +
-  geom_text(aes(label=paste0(Percentage,"%"))) +
-  labs(x = "Sport Gender", title = "Comparing NCAA Men's and Women's Division I Sports by Coach's Gender", fill = "Coach Gender") +
-  scale_y_continuous(labels = scales::percent_format())
+# ggplot(summaryData, aes(x = Sport.Gender, y = Percentage, fill = Coach.Gender)) + 
+#   geom_bar(position = "fill", stat="identity") +
+#   geom_text(aes(x = Sport.Gender, y = pos, label=paste0(Percentage,"%")), position = position_stack(vjust=0.1)) +
+#   labs(x = "Sport Gender", title = "Comparing NCAA Men's and Women's Division I Sports by Coach's Gender", fill = "Coach Gender") +
+#   scale_y_continuous(labels = scales::percent_format())
 
+
+fill <- c("#5F9EA0", "#E1B378")
+ggplot() + 
+  geom_bar(data=summaryData, aes(x=Sport.Gender, y=Percentage, fill=Coach.Gender), stat="identity") +
+  geom_text(data=summaryData, aes(x=Sport.Gender, y=pos, label=paste0(Percentage,"%"))) +
+  labs(x = "Sport Gender", title = "Comparing NCAA Men's and Women's Division I Sports by Coach's Gender", fill = "Coach Gender") +
+  scale_y_continuous(labels = dollar_format(suffix = "%", prefix="")) +
+  scale_fill_manual(values=fill)
